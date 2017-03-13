eaa797eb04d69f5abf3e0c0fc64c830b67fb6e7f
gglinux <gglinux@163.com>
2017-03-13 18:32:27 +0800

    fix

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -4,12 +4,12 @@ namespace api\controllers;
 
 use Yii;
 use yii\filters\AccessControl;
-use yii\web\Controller;
+use api\base\ApiController;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
 
-class SiteController extends Controller
+class SiteController extends ApiController
 {
     /**
      * @inheritdoc
